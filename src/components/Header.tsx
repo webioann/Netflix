7ec9f1a95ccc99717693593a222a61b370426e1d
Webioann <ioannn7777@gmail.com>
2022-12-13 16:42:34 +0200

    clear some style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,15 @@ import '../style/header.scss'
 const Header = () => {
     return (
         <header className='header'>
-            <img className='netflix-logo'
-                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
-                alt="Netflix logo"/>
-            <img className='avatar-logo'
-                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
-                alt="avatar logo"/>
+            <div className='header-wrapper'>
+                <img className='netflix-logo'
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+                    alt="Netflix logo"/>
+                <img className='avatar-logo'
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
+                    alt="avatar logo"/>
+
+                </div>
         </header>
     )
 }
