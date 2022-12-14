07af7cd0a5374f764b0d77f540d88bb9dd3ea417
Webioann <ioannn7777@gmail.com>
2022-12-14 23:44:41 +0200

    link on home page with click on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,10 +12,11 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='header-wrapper'>
+                <Link to={'/'}>
                 <img className='netflix-logo'
                     src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
                     alt="Netflix logo"/>
-                <Link to={'/profile'}>Profile</Link>  
+                </Link>  
                 { currentUser && <SignoutButton/> }
                 <Link to={'/login'} className='header-login-link'>
                     <img className='avatar-logo'
