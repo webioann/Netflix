fcdc423886407d94f31b68e248ca5e8baa4035a4
Webioann <ioannn7777@gmail.com>
2023-01-06 00:08:22 +0200

    stylished MyListPage

--- src/components/MyListPage.tsx
+++ src/components/MyListPage.tsx
@@ -19,21 +19,28 @@ const MyListPage = () => {
     const [myListMovies, setMyListMovies] = useState<IMyListMovies[] | []>([])
 
     useEffect(() => {
+        // onSnapshot(collection(db, "my list"), (snapshot) => {
+        //     // console.log(snapshot.docs.map(doc => doc.data()));
+        //     const results = []
+        //     setMyListMovies(snapshot.docs.map((doc) => {...doc.data(), doc_id: doc.id}))
+        // })
+
         const fetchMyList = async () => {
             const data = await getDocs(collection(db, "my list"));
-            // const dataUp = await onSnapshot(doc(db, "my list"), (movies) => {
-            //     setMyListMovies(movies.map((doc) => ({...doc.data(), doc_id: doc.id }) ))
+            // onSnapshot(collection(db, "my list"), (snapshot) => {
+            //     // console.log(snapshot.docs.map(doc => doc.data()));
+            //     setMyListMovies(snapshot.docs.map((doc) => ({doc.data(), doc_id: doc.id})))
             // })
             // let raw: IMyListMovies[] | [] = [];
             // data.forEach((doc) => {
-            //     raw.push({...doc.data(), doc_id: doc.id })
+            //     setMyListMovies((prevState) => [...prevState, {...doc.data(), doc_id: doc.id}]);
             // })
-            setMyListMovies(data.docs.map((doc) => ({...doc.data(), doc_id: doc.id }) ))
+            setMyListMovies(data.docs.map((doc) => ({...doc.data(), doc_id: doc.id })))
         }
         fetchMyList();
     }, [])
 
-// console.log(myListMovies)
+console.log(myListMovies)
     return (
         <div className='my-list-page'
             style={{backgroundImage: `url(${PAGE_BG_IMG})`}}
@@ -42,47 +49,47 @@ const MyListPage = () => {
                 <h1>My List</h1>
                 <ul className="my-list-content">
                     { myListMovies.map(movie => (
-                                <li className='movie-card'>
-                                <img className='movie-card-img'
-                                    src={`${IMG_BASE_URL}${movie.backdrop_path ? movie.backdrop_path : movie.poster_path}`} 
-                                    alt={ movie.media_type === 'movie' ? movie.title : movie.name }
-                                />
-                                <div className="movie-card-controls">
-                                    <div className="poster-controls-info">
-                                        <button className="poster-play-button">
-                                            <PlayVideoButtonIcon 
-                                                size={20} 
-                                                color='#fff' 
-                                                media_type={movie.media_type} 
-                                                movie_id={movie.id}
-                                            />
-                                        </button>
-                                        <p className='movie-name'>
-                                            { movie.media_type === 'movie' ? movie.title : movie.name }
-                                        </p>
-                                        <GenresList genres={movie?.genre_ids} font={12}/>
-                                    </div>
-                    
-                                    <div className="poster-controls-buttons">
-                                        <button className="movie-slider-circle">
-                                            <HiVolumeOff size={17} color='rgba(255, 255, 255, 0.7)'/>
-                                        </button>
-                                        {/* <button className="movie-slider-circle">
-                                            <AiOutlineLike size={17} color='rgba(255, 255, 255, 0.7)'/>
-                                        </button> */}
-                                        <button className="movie-slider-circle">
-                                            <AiOutlineDislike size={17} color='rgba(255, 255, 255, 0.7)'/>
-                                        </button>
-                                        <button 
-                                            className="movie-slider-circle"
-                                            onClick={() => deleteMovieFromMyList(movie.doc_id)}
-                                            >
-                                            <FaCheck size={17} color='rgba(255, 255, 255, 0.7)' title='remove from My List'/>
-                                        </button>
-                    
-                                    </div>
+                        <li className='my-list-movie' key={movie.id}>
+                            <img className='my-list-movie-img'
+                                src={`${IMG_BASE_URL}${movie.backdrop_path ? movie.backdrop_path : movie.poster_path}`} 
+                                alt={ movie.media_type === 'movie' ? movie.title : movie.name }
+                            />
+                            <div className="my-list-controls">
+                                <div className="controls-info">
+                                    <button className="play-button">
+                                        <PlayVideoButtonIcon 
+                                            size={20} 
+                                            color='#fff' 
+                                            media_type={movie.media_type} 
+                                            movie_id={movie.id}
+                                        />
+                                    </button>
+                                    <p className='movie-name'>
+                                        { movie.media_type === 'movie' ? movie.title : movie.name }
+                                    </p>
+                                    <GenresList genres={movie?.genre_ids} font={12}/>
                                 </div>
-                            </li>
+                
+                                <div className="my-list-buttons">
+                                    <button className="circle-button">
+                                        <HiVolumeOff size={17} color='rgba(255, 255, 255, 0.7)'/>
+                                    </button>
+                                    {/* <button className="movie-slider-circle">
+                                        <AiOutlineLike size={17} color='rgba(255, 255, 255, 0.7)'/>
+                                    </button> */}
+                                    <button className="circle-button">
+                                        <AiOutlineDislike size={17} color='rgba(255, 255, 255, 0.7)'/>
+                                    </button>
+                                    <button 
+                                        className="circle-button"
+                                        onClick={() => deleteMovieFromMyList(movie.doc_id)}
+                                        >
+                                        <FaCheck size={17} color='rgba(255, 255, 255, 0.7)' title='remove from My List'/>
+                                    </button>
+                
+                                </div>
+                            </div>
+                        </li>
                     ))}
                 </ul>
             </Container>
