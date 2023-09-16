ce8fe9ba63c3cf1809ad86dab339999610715871
Webioann <ioannn7777@gmail.com>
2023-09-16 18:34:18 +0300

    SignOut button in accoun

--- src/pages/Account_Page.tsx
+++ src/pages/Account_Page.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import UserAvatar from '../components/UserAvatar'
+import Signout_Button from '../components/Signout_Button'
 import { UserContext } from '../hooks/USER_CONTEXT_PROVIDER'
 import '../style/account-page.scss'
 
@@ -115,6 +116,7 @@ const Account_Page = () => {
                         </div>
                     </div>
                 </div>
+                <Signout_Button/>
             </section>
         </main>
     )
