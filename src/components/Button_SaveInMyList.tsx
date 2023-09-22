79a6fcf1325d06df7b92bd13a62b474efadbe99b
Webioann <ioannn7777@gmail.com>
2023-09-22 11:11:02 +0300

    more correct tooltip title for SaveInMyList

--- src/components/Button_SaveInMyList.tsx
+++ src/components/Button_SaveInMyList.tsx
@@ -17,18 +17,22 @@ interface IParamsOnSave {
 const Button_SaveMovieInMyList: React.FC<ISaveMovieInMyList> = ({ movie, title }) => {
 
     const [isSaved, setIsSaved] = useState(false)
+    const [tooltipTitle, setTooltipTitle] = useState('login for save this movie in My List')
+
     const {user} = useContext(UserContext)
 
     useEffect(() => {
         if( user?.email ) {
             let userList = user?.email?.toString()
             const list = localStorage.getItem(userList)
+            setTooltipTitle('click for save in My List')
             if( list ) {
                 let data: IMovie[] = JSON.parse(list)
                 setIsSaved(data.some((item) => item.id === movie.id))
             }
             else{ setIsSaved(false) }
         }
+        else{ setTooltipTitle('login for save this movie in My List') }
     })
 
     const saveMovieInMyList = async ({ movie }: IParamsOnSave) => {
@@ -57,8 +61,8 @@ const Button_SaveMovieInMyList: React.FC<ISaveMovieInMyList> = ({ movie, title }
             className={ title ? 'square-button' : 'small-circle circle-button'}
             >
             <i>{ isSaved && user
-                ? <AiOutlineCheck size={12} color='#fff' title='you save thise movie'/> 
-                : <AiOutlinePlus size={12} color='#fff' title='save in My List'/>}
+                ? <AiOutlineCheck size={12} color='#fff' title='this movie saved in My List'/> 
+                : <AiOutlinePlus size={12} color='#fff' title={tooltipTitle}/>}
             </i>
             { title }
         </button>
